4e4bd53687f889220d2722e7060b7cda3fb10978
Jason Thompson <jason@jthompson.ca>
2012-06-28 06:28:45 -0400

    finished Reader

--- lib/jekyll-converter/reader.rb
+++ lib/jekyll-converter/reader.rb
@@ -2,12 +2,30 @@ require 'yaml'
 
 module JekyllConverter
   class Reader
-    attr_reader :title, :tags
+    attr_reader :title, :tags, :body
+
     def initialize(input_io)
-      metadata = YAML::load(input_io)
-      @title = metadata['title']
-      @tags = metadata['categories']
-      @date = metadata['date']
+      content = input_io.read
+
+      read_body_and_data(content)
+
+      @title = @data['title']
+      @tags = @data['categories']
     end
+
+    private
+
+    def read_body_and_data(content)
+      # following code lovingly adapted from Jekyll
+      begin
+        if content =~ /^(---\s*\n.*?\n?)^(---\s*$\n?)/m # matches everything between --- and ---
+          @body = $' # Everything after the match
+          @data = YAML.load($1)
+        end
+      rescue => e
+        puts "YAML Exception"
+      end
+    end
+
   end
 end
---
+++ spec/.#reader_spec.rb
@@ -0,0 +1,1 @@
+jason@Jasons-iMac.local.68139
